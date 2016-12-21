64a4c71a1fb0277b1874f1b415bd39f0f73def38
jklingstedt <jklingstedt@jawbone.com>
2016-12-21 14:59:56 -0800

    react and routing done

---
+++ app/containers/Main/MainContainer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+class MainContainer extends Component {
+    render () {
+        return (
+            <div>
+                Hello Ass
+            </div>
+        )
+    }
+}
+
+export default MainContainer
